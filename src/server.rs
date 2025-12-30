861e837ec876962e6e3a2aa9cc72a6768413cc85
Emily S <emilywselwood@gmail.com>
2025-12-30 20:05:04 +0000

    file hosting outside target and remove unused code

--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,9 @@
-use std::{env, path::PathBuf, time::Duration};
+use std::{env, path::PathBuf, sync::Mutex, time::Duration};
 
 use actix_files::NamedFile;
 use actix_session::{SessionMiddleware, storage::CookieSessionStore};
 use actix_web::{App, HttpRequest, HttpServer, Result, cookie::Key, middleware::Logger, web};
-use anyhow::{Context, Error};
+use anyhow::{Context, Error, anyhow};
 use log::{debug, info};
 
 use crate::{config::Config, error::WebError};
@@ -29,7 +29,7 @@ pub async fn server_main(settings: &Config) -> Result<(), Error> {
         config: settings.clone(),
     };
 
-    let web_data = web::Data::new(app_data);
+    let web_data = web::Data::new(Mutex::new(app_data));
 
     let server = HttpServer::new(move || {
         App::new()
@@ -39,13 +39,14 @@ pub async fn server_main(settings: &Config) -> Result<(), Error> {
                 secret_key.clone(),
             ))
             .app_data(web_data.clone())
-            // General file routes. images, css, and javascript
+            // General file routes. images, css, fonts, and javascript
             .route(
-                "/img/{filename:.*\\.(jpg|png|webp)}",
+                "/img/{filename:.*\\.(jpg|png|webp|gif)}",
                 web::get().to(image_host),
             )
             .route("/css/{filename:.*\\.css}", web::get().to(css_host))
             .route("/scripts/{filename:.*\\.js}", web::get().to(script_host))
+            .route("/fonts/{filename:.*\\.ttf}", web::get().to(font_host))
             // The index page routes
             .route("/index.html", web::get().to(index_page))
             .route("/", web::get().to(index_page))
@@ -61,9 +62,15 @@ pub async fn server_main(settings: &Config) -> Result<(), Error> {
     Ok(())
 }
 
-async fn image_host(req: HttpRequest) -> Result<NamedFile, WebError> {
-    let root_path: PathBuf =
-        get_root_path("static/open/img/").context("could not get root path")?;
+async fn image_host(
+    req: HttpRequest,
+    data: web::Data<Mutex<AppData>>,
+) -> Result<NamedFile, WebError> {
+    let app_data = data
+        .lock()
+        .map_err(|_e| anyhow!("Could not lock AppData Mutex in image_host."))?;
+    let root_path: PathBuf = get_root_path("static/open/img/", &app_data.config)
+        .context("could not get image root path")?;
     let path: PathBuf = req
         .match_info()
         .query("filename")
@@ -75,9 +82,15 @@ async fn image_host(req: HttpRequest) -> Result<NamedFile, WebError> {
     Ok(NamedFile::open(full_path).context("could not open file")?)
 }
 
-async fn css_host(req: HttpRequest) -> Result<NamedFile, WebError> {
-    let root_path: PathBuf =
-        get_root_path("static/open/css/").context("could not get root path")?;
+async fn css_host(
+    req: HttpRequest,
+    data: web::Data<Mutex<AppData>>,
+) -> Result<NamedFile, WebError> {
+    let app_data = data
+        .lock()
+        .map_err(|_e| anyhow!("Could not lock AppData Mutex in css_host."))?;
+    let root_path: PathBuf = get_root_path("static/open/css/", &app_data.config)
+        .context("could not get css root path")?;
     let path: PathBuf = req
         .match_info()
         .query("filename")
@@ -89,9 +102,15 @@ async fn css_host(req: HttpRequest) -> Result<NamedFile, WebError> {
     Ok(NamedFile::open(full_path).context("could not open file")?)
 }
 
-async fn script_host(req: HttpRequest) -> Result<NamedFile, WebError> {
-    let root_path: PathBuf =
-        get_root_path("static/open/scripts/").context("could not get root path")?;
+async fn script_host(
+    req: HttpRequest,
+    data: web::Data<Mutex<AppData>>,
+) -> Result<NamedFile, WebError> {
+    let app_data = data
+        .lock()
+        .map_err(|_e| anyhow!("Could not lock AppData Mutex in script_host."))?;
+    let root_path: PathBuf = get_root_path("static/open/scripts/", &app_data.config)
+        .context("could not get scripts root path")?;
     let path: PathBuf = req
         .match_info()
         .query("filename")
@@ -103,21 +122,57 @@ async fn script_host(req: HttpRequest) -> Result<NamedFile, WebError> {
     Ok(NamedFile::open(full_path).context("Could not open file")?)
 }
 
-async fn index_page(_req: HttpRequest) -> Result<NamedFile, WebError> {
-    let index_page_path: PathBuf =
-        get_root_path("static/open/index.html").context("could not get root path")?;
-    debug!("trying to serve index page {}", index_page_path.display());
-    Ok(NamedFile::open(index_page_path).context("could not open file")?)
+async fn font_host(
+    req: HttpRequest,
+    data: web::Data<Mutex<AppData>>,
+) -> Result<NamedFile, WebError> {
+    let app_data = data
+        .lock()
+        .map_err(|_e| anyhow!("Could not lock AppData Mutex in font_host."))?;
+    let root_path: PathBuf =
+        get_root_path("static/fonts/", &app_data.config).context("could not get font root path")?;
+    let path: PathBuf = req
+        .match_info()
+        .query("filename")
+        .parse()
+        .context("could not parse filename")?;
+    let full_path = root_path.join(&path);
+
+    debug!("trying to serve font {}", &full_path.display());
+    Ok(NamedFile::open(&full_path)
+        .with_context(|| format!("Could not open font file {:?}", &full_path))?)
 }
 
-fn get_root_path(path: &str) -> Result<PathBuf, Error> {
+async fn index_page(
+    _req: HttpRequest,
+    data: web::Data<Mutex<AppData>>,
+) -> Result<NamedFile, WebError> {
+    let app_data = data
+        .lock()
+        .map_err(|_e| anyhow!("Could not lock AppData Mutex in index_page."))?;
+    let index_page_path: PathBuf = get_root_path("static/open/index.html", &app_data.config)
+        .context("could not get index root path")?;
+    debug!("trying to serve index page {}", index_page_path.display());
+    Ok(NamedFile::open(index_page_path).context("could not open index file")?)
+}
+
+fn get_root_path(path: &str, config: &Config) -> Result<PathBuf, Error> {
+    // TODO: some how decide if we are going from the exe or the current working directory
+    // so we can have hot reloading of assets for development
+    let root_path = if config.debug {
+        env::current_dir()
+            .context("Could not get current dir")?
+            .to_path_buf()
+            .join("resources")
+    } else {
+        env::current_exe()
+            .context("Could not get current exe")?
+            .parent()
+            .context("could not get parent of current exe")?
+            .to_path_buf()
+    };
     let sub_path: PathBuf = path
         .parse()
         .with_context(|| format!("could not parse path {}", path))?;
-    Ok(env::current_exe()
-        .context("Could not get current exe")?
-        .parent()
-        .context("could not get parent of current exe")?
-        .to_path_buf()
-        .join(sub_path))
+    Ok(root_path.join(sub_path))
 }
